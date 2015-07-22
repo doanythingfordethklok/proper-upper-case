0d972120c0b0a53d548960765f25c0e27ff7312a
Some Web Media <somewebmedia@gmail.com>
2015-07-22 13:38:22 +0200

    removed lodash dependency and simplified

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-var _ = require('lodash');
-
 module.exports = function(str) {
-  var fstr = (str || '').toString().toLowerCase().split(/\s+/g);
-
-  fstr = _.map(fstr, function(s) {
-    return (s[0] || '').toUpperCase() + s.slice(1, s.length);
-  }).join(' ');
-
-  return fstr;
+  str = (str || '').toString().toLowerCase();
+  return str.replace(/(?:^|\s)\S/g, function(s) {
+    return s.toUpperCase();
+  });
 };
